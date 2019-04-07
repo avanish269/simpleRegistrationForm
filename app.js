f723686bbd3e86d3ef07c96daec81ab150c26ecb
avanish269 <avanish269@gmail.com>
2019-04-08 01:26:26 +0530

    Added views using pug template engine

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express'); //exporting express module
 const routes = require('./routes/index'); //exporting route files into the app
+const path = require('path');
 
 const app = express(); //Creating a new express app and assign it to app variable or in this case constant
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 app.use('/',routes); //whenever app receives request from /anything it uses routes file
 
 module.exports = app; //export app to be used by other files
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express'); //importing express
 const router = express.Router(); //creating router
 
 router.get('/', function(req,res){
-    res.send("Routing is working"); //sending message
+    res.render('form', {title:'Registration Form'}); //sending message
 });
 
 module.exports = router;
